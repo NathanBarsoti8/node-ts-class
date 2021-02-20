197d7527a62968d177417f5896a51efdd53aba4a
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-02-20 12:52:52 -0300

    call routes on server

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import path from 'path';
 import logging from './config/logging';
 import config from './config/env';
+import routes from './index';
 
-const namespace = process.env.NAMESPACE || 'PROVIDERS API';
+const namespace = 'PROVIDERS API';
 const app = express();
 
 const env = process.env.NODE_ENV || 'development';
 
 config(env, app);
+routes(app);
 
 app.listen(app.get('port'), () => {
     logging.info(namespace, `LISTENING ON PORT ${app.get('port')} `);
