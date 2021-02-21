acb559090d3e60ad5d8ffb386e92fca8a8da162f
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-02-21 11:54:27 -0300

    providers interface and model

---
+++ src/interfaces/providers.ts
@@ -0,0 +1,42 @@
+import { Document } from 'mongoose';
+
+interface IProviders extends Document {
+    parentCnpjNumber: string;
+    cnpjNumber: string;
+    providerName: string;
+    // fantasyName: string;
+    // stateRegistration: string;
+    // municipalRegistration: string;
+    // addresses: Array<IAdress>;
+    // contacts: Array<IContact>;
+    // companies: Array<ICompany>;
+    // active: string;
+    // skusCount: string;
+    // createdAt: Date;
+    // updatedAt: Date;
+}
+
+interface IAdress extends Document {
+    typeAddress: string;
+    zipCode: string;
+    country: string;
+    state: string;
+    city: string;
+    street: string;
+    number: string;
+    complement: string;
+}
+
+interface IContact extends Document {
+    fullName: string;
+    telephoneNumber: string;
+    emailAddress: string;
+    department: string;
+}
+
+interface ICompany extends Document {
+    companyId: string;
+    companyName: string;
+}
+
+export default IProviders;
---
+++ src/models/providers.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from 'mongoose';
+import IProviders from '../interfaces/providers';
+
+const providers: Schema = new Schema({
+    parentCnpjNumber: { type: String, required: true },
+    cnpjNumber: { type: String, required: true },
+    providerName: { type: String, required: true }
+    // fantasyName: { type: String, required: true },
+    // stateRegistration: { type: String, required: false },
+    // municipalRegistration: { type: String, required: false },
+    // addresses: [address],
+    // contacts: [contact],
+    // companies: [company],
+    // createdAt: { type: Date, required: false },
+    // updatedAt: { type: Date, required: false },
+    // active: { type: String, required: false },
+    // skusCount: { type: String, required: false }
+});
+
+// const address: Schema = new Schema({
+//     typeAddress: { type: String, required: true },
+//     zipCode: { type: String, required: true },
+//     country: { type: String, required: true },
+//     state: { type: String, required: true },
+//     city: { type: String, required: true },
+//     street: { type: String, required: true },
+//     number: { type: String, required: true },
+//     complement: { type: String, required: true }
+// });
+
+// const contact: Schema = new Schema({
+//     fullName: { type: String, required: true },
+//     telephoneNumber: { type: String, required: true },
+//     emailAddress: { type: String, required: true },
+//     department: { type: String, required: true }
+// });
+
+// const company: Schema = new Schema({
+//     companyId: { type: String, required: true },
+//     companyName: { type: String, required: true }
+// });
+
+export default mongoose.model<IProviders>('providers', providers);
