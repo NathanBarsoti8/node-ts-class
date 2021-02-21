91a97f9849066db44fc7f36ebd4e39e370144def
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-02-21 11:36:58 -0300

    add connection with db

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,8 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import logging from '../config/logging';
 
 const config = (env: string, app: any) => {
     const port = process.env.PORT || 3333;
@@ -8,6 +11,22 @@ const config = (env: string, app: any) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cors());
+    app.options('*', cors());
+    app.use(morgan('combined'));
+
+    mongoose
+        .connect('mongodb://localhost:27017/db_providers', {
+            useFindAndModify: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        .then(() => {
+            logging.info('DB', 'database successfully connected');
+        })
+        .catch((err) => {
+            logging.error('DB', err.message, err);
+        });
 };
 
 export default config;
