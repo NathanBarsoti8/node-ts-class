871b7a2078fb0accf05985bf97dd958cf3970736
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-02-22 09:02:04 -0300

    list method

--- src/controllers/providers/listAll.ts
+++ src/controllers/providers/listAll.ts
@@ -1,14 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import logging from '../../config/logging';
+import Providers from '../../models/providers';
 
 const namespace = 'CONTROLLERS => PROVIDERS => LIST ALL';
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export default async (req: Request, res: Response) => {
     logging.info(namespace, 'list all method called');
 
     try {
-        return res.status(200).json({ message: 'ok' });
+        const providers = await Providers.find();
+
+        if (!providers || providers.length == 0) {
+            return res.status(404).json({ message: 'not found results according these parameters' });
+        }
+
+        return res.status(200).json({
+            content: providers,
+            count: providers.length
+        });
     } catch (err) {
-        return res.status(400).json({ message: err.message });
+        return res.status(500).json({ message: err.message });
     }
 };
