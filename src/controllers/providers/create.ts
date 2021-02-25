a0c26877ec4d47a4f29e8c6978e7d19db132cf88
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-02-25 11:36:15 -0300

    cnpj validator

--- src/controllers/providers/create.ts
+++ src/controllers/providers/create.ts
@@ -1,23 +1,43 @@
 import { Request, Response } from 'express';
 import logging from '../../config/logging';
-import Providers from '../../models/providers';
+import IProviders from '../../interfaces/providers';
+import STATUS from '../../enum/httpStatusCode';
+import ProviderRepository from '../../repositories/providers';
+import { cnpj } from 'cpf-cnpj-validator';
 
 const namespace = 'CONTROLLERS => PROVIDERS => CREATE';
 
-export default (req: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
     logging.info(namespace, 'create method called');
+
+    const providerRepository = new ProviderRepository();
 
     try {
         const { parentCnpjNumber, cnpjNumber, providerName } = req.body;
 
-        Providers.create({
+        const validCNPJ: boolean = cnpj.isValid(cnpjNumber);
+
+        if (!validCNPJ) {
+            return res.status(STATUS.BAD_REQUEST).json({ message: 'invalid cnpj' });
+        }
+
+        const exists: boolean = await providerRepository.cnpjExists(cnpjNumber);
+
+        if (exists) {
+            return res.status(400).json({ message: 'this cnpj already exists in database' });
+        }
+
+        const provider = {
             parentCnpjNumber: parentCnpjNumber,
             cnpjNumber: cnpjNumber,
-            providerName: providerName
-        }).then(() => {
-            return res.status(201).json({ message: 'provider successfully created' });
-        });
+            providerName: providerName,
+            active: 'true'
+        } as IProviders;
+
+        const response = await providerRepository.create(provider);
+
+        return res.status(response.status).json({ message: response.message, obj: response.obj });
     } catch (err) {
-        return res.status(400).json({ message: err.message });
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: err.message });
     }
 };
